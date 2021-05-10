3c7bee192d721e03637c0fa8c6c334b04c1901ed
Jayakumar <Jayakumar@Jayakumar-PC>
2021-05-10 13:24:34 +0530

    Implemented the executors service for increasing the performance

--- src/main/java/com/password/validation/constant/ErrorConstantMessage.java
+++ src/main/java/com/password/validation/constant/ErrorConstantMessage.java
@@ -15,4 +15,5 @@ public class ErrorConstantMessage {
 	public static final String PASSWORD_SHOULD_NOT_NULL = "password should not be null";
 	public static final String PASSWORD_SHOULD_HAVE_AT_LEAST_ONE_LOWERCASE_LETTER = "password should have one lower letter at least";
 	public static final String PASSWORD_SHOULD_HAVE_AT_LEAST_ONE_UPPERCASE_CHAR_OR_ONE_NUMBER_OR_ATLEAST_EIGHT_CHAR = "password should have at least one uppercase letter or number or atleast 8 character";
+	public static final String INTERNAL_SERVER_ERROR = "Internal server error";
 }
--- src/main/java/com/password/validation/service/impl/PasswordValidationServiceImpl.java
+++ src/main/java/com/password/validation/service/impl/PasswordValidationServiceImpl.java
@@ -10,18 +10,29 @@ package com.password.validation.service.impl;
 import static com.password.validation.constant.ErrorConstantMessage.*;
 import static com.password.validation.util.ValidationUtil.*;
 
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+import com.password.validation.exception.ApplicationException;
 import com.password.validation.exception.PasswordValidationException;
 import com.password.validation.service.PasswordValidationService;
 
 public class PasswordValidationServiceImpl implements PasswordValidationService {
 
+	ExecutorService executorsService = Executors.newFixedThreadPool(5);
+	
 	public String validatePassword(final String password) throws PasswordValidationException {
 
-		if (isNull.test(password)) {
-			throw new PasswordValidationException(PASSWORD_SHOULD_NOT_NULL);
-		}
+		Future<String> result = executorsService.submit(() -> {
+			if (isNull.test(password)) {
+				throw new PasswordValidationException(PASSWORD_SHOULD_NOT_NULL);
+			}
+			return checkPasswordHasMandatoryLetter(password);
+		});
 
-		return checkPasswordHasMandatoryLetter(password);
+		return getValueFromFutureObject(result);
 	}
 	
 	/**
@@ -39,6 +50,27 @@ public class PasswordValidationServiceImpl implements PasswordValidationService 
 		} else {
 			throw new PasswordValidationException(
 					PASSWORD_SHOULD_HAVE_AT_LEAST_ONE_UPPERCASE_CHAR_OR_ONE_NUMBER_OR_ATLEAST_EIGHT_CHAR);
+		}
+	}
+	
+	/**
+	 * Method for getting the value from the future object
+	 * 
+	 * @param result
+	 * @return
+	 */
+	private String getValueFromFutureObject(Future<String> result) {
+		try {
+			return result.get();
+		} catch (InterruptedException e) {
+			Thread.currentThread().interrupt();
+			throw new ApplicationException(INTERNAL_SERVER_ERROR);
+		} catch (ExecutionException e) {
+			if (e.getCause().getClass() == PasswordValidationException.class) {
+				throw new PasswordValidationException(e.getCause().getMessage());
+			} else {
+				throw new ApplicationException(INTERNAL_SERVER_ERROR);
+			}
 		}
 	}
 
